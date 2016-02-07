08bb876501211614095f5b5348aa2181b2b6c6e3
gieses <sven.giese88@gmail.com>
2016-02-07 21:11:03 +0100

    [enhancement] cleaned code a bit and started figure design

--- CreateFigures.py
+++ CreateFigures.py
@@ -8,6 +8,9 @@ Created on Sun Jan 31 22:33:32 2016
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+sns.set_style("ticks")
+sns.set_context("talk", font_scale=1.8)
 
 def fig_phosints():
     """
@@ -17,15 +20,25 @@ def fig_phosints():
 
 sheetnames = ["log10HL", "log10HM", "log10ML"]
 for sheet in sheetnames:
-    excel = pd.read_excel("D:\Sven\Dropbox\VX data for Sven\SVEN\data\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide_overview.xlsx", sheetname=sheet)
-    break
+    phospho = pd.read_excel("D:\Sven\Dropbox\VX data for Sven\SVEN\data\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide_overview.xlsx", sheetname=sheet)
+    regular = pd.read_excel("D:\Sven\Dropbox\VX data for Sven\SVEN\data\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide\ALL162files_Mit_Int_CytoPurA_mix_SCX_TiO2_peptide_nonhospho_overview.xlsx", sheetname=sheet)
+    column = "norm_"+sheet
 
-
-f, ax = plt.subplots(1, figsize=(11.69, 8.27))
-ax.plot()
-
-excel.columns
-
+    #%%
+    f, ax = plt.subplots(1, figsize=(11.69, 8.27))
+    ax.scatter(regular[column],
+                np.log10(regular["Area"]), s=80, lw=1, alpha=0.7,
+                label="Regular peptides")
+    ax.scatter(phospho[column],
+                np.log10(phospho["Area"]), c="green", s=80, lw=1,
+                alpha=0.7, label="phospho peptides")
+    ax.set(title="Normalized peptide Ratios: {}".format(sheet),
+           xlabel="log2 (fold change)", ylabel="log10 (intensity)",
+           xlim=(-3, 3))
+    ax.legend(loc="upper left")
+    sns.despine()
+    plt.show()
+    #%%
 
 plt.hist(excel[excel["direction"]=="Down"]["norm_log10HL"].values, bins=20, label="Down")
 plt.hist(excel[excel["direction"]=="Up"]["norm_log10HL"].values, bins=20, label="Up")
@@ -55,17 +68,4 @@ plt.hist(regular_filtered["log10HM"].values, bins=30)
 plt.hist(phospho_filtered["norm_log10HM"].values, bins=30)
 plt.xlim(-1.2, 1.2)
 
-sns.set_style("ticks")
-sns.set_context("talk", font_scale=1.4)
-plt.scatter(regular_filtered["norm_log10HM"],
-            np.log10(regular_filtered["Area"]), s=80, lw=1, alpha=0.7,
-            label="Regular peptides")
-plt.scatter(phospho_filtered["norm_log10HM"],
-            np.log10(phospho_filtered["Area"]), c="green", s=80, lw=1,
-            alpha=0.7, label="phospho peptides")
-plt.title("Normalized peptide Ratios: HM")
-plt.xlabel("log2 (fold change)")
-plt.ylabel("log10 (intensity)")
-plt.xlim(-3, 3)
-sns.despine()
-plt.show()
+
